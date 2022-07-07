414f55f43fd26955e54869a8b84c18691d260496
XandaoTP <alexandre8603@gmail.com>
2022-07-07 14:37:28 -0300

    inicia calculadora

---
+++ front-end/src/calculator/index.jsx
@@ -0,0 +1,61 @@
+import styled from '@emotion/styled';
+import Container from '@mui/material/Container';
+import { Box } from '@mui/system';
+import { useState } from 'react';
+
+export function Calculator () {
+    const [som, setSom] = useState(0)
+    function InputNum(e) {
+        const input = e.target.value 
+        if(som === 0) {
+            setSom(input) 
+        } else{
+        setSom(som + input)
+        }
+    }
+    function porcent () {
+        setSom(som/100)
+    }
+    function clearCalc () {
+        setSom(0)
+    }
+    return (
+        <>
+        <Box m={3}>
+        <Container fixed maxWidth="xs">
+        <DivStyled>
+            <H1Styled>{som}</H1Styled>
+            <button onClick={InputNum} value={1}>1</button>
+            <button onClick={InputNum} value={2}>2</button>
+            <button onClick={InputNum} value={3}>3</button>
+            <button onClick={clearCalc}>C</button>
+            <button onClick={InputNum} value={4}>4</button>
+            <button onClick={InputNum} value={5}>5</button>
+            <button onClick={InputNum} value={6}>6</button>
+            <button>X</button>
+            <button onClick={InputNum} value={7}>7</button>
+            <button onClick={InputNum} value={8}>8</button>
+            <button onClick={InputNum} value={9}>9</button>
+            <button>+</button>
+            <button>,</button>
+            <button onClick={InputNum} value={0}>0</button>
+            <button>/</button>
+            <button>-</button>
+            <button onClick={porcent}>%</button>
+            <button>=</button>
+        </DivStyled>
+        </Container>
+        </Box>
+        </>
+    )
+}
+
+const DivStyled = styled.div`
+    height: 250px;
+    width: 110px;
+    background-color: black;
+    
+`
+const H1Styled = styled.h1`
+color: white;
+`
